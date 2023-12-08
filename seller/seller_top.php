0c9de08a69e71d919490f5929006f309bc81bc54
meru83 <rion160424@gmail.com>
2023-12-08 12:12:31 +0900

    ログイン済みと未ログインで状態を分ける

--- seller/seller_top.php
+++ seller/seller_top.php
@@ -1,3 +1,13 @@
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', 'error.log'); // エラーログをerror.logファイルに記録
+error_reporting(E_ALL);
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -7,11 +17,33 @@
 </head>
 <body>
     <h1>売り手側</h1>
-    <form action="seller_out.php" method="post">
-        <input type="submit" name="logout" value="ログアウト">
-    </form>
+    <?php
+    if(isset($_SESSION['seller_id'])){
+        echo <<<END
+        <form action="seller_out.php" method="post">
+            <input type="submit" name="logout" value="ログアウト">
+        </form>
+        END;
+    }else{
+        echo <<<END
+        <a href="seller.php">新規登録</a>
+        <a href="seller_log.php">ログイン</a>
+        <br>
+        <br>
+        END;
+    }
+    ?>
 
-    <a href="p2_insert.php">商品情報登録</a><br>
-    <a href="../chat_rooms.php">チャットルーム一覧</a>
+    <?php
+    if(isset($_SESSION['seller_id'])){
+        echo <<<END
+        <a href="p2_insert.php">商品情報登録</a><br>
+        <a href="seller_products.php">登録済み商品一覧</a><br>
+        <a href="../chat_rooms.php">チャットルーム一覧</a>
+        END;
+    }else{
+        echo "ユーザー登録またはログインを完了させてください。";
+    }
+    ?>
 </body>
 </html>
