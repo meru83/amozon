a36447ef675d298d98591a51aeca06f9dd5fdc06
meru83 <rion160424@gmail.com>
2023-12-08 16:06:19 +0900

    css追加;

--- seller/seller_top.php
+++ seller/seller_top.php
@@ -14,6 +14,7 @@ error_reporting(E_ALL);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>売り手のトップ</title>
+    <link rel="stylesheet" href="../css/Amozon_insta.css">
 </head>
 <body>
     <h1>売り手側</h1>
@@ -36,6 +37,7 @@ error_reporting(E_ALL);
 
     <?php
     if(isset($_SESSION['seller_id'])){
+        //以下のリンク対応させてね!!!!!!!!!!!!!
         echo <<<END
         <a href="p2_insert.php">商品情報登録</a><br>
         <a href="seller_products.php">登録済み商品一覧</a><br>
@@ -45,5 +47,33 @@ error_reporting(E_ALL);
         echo "ユーザー登録またはログインを完了させてください。";
     }
     ?>
+    <div class="Amozon-container">
+        <!-- Left Side Menu -->
+        <div class="left-menu">
+            <div>
+                <ul class="menu-list">
+                    <li class="menu-item"><a href=""><img src="../img/cart_dake.svg" class="logo"><span class="menu-item-text-logo">Re.ReaD</span></a></li>
+                    <li class="menu-item"><a href=""><img src="../img/home.png" class="logo"></span><span class="menu-item-text">ホーム</span></a></li>
+                    <li class="menu-item"><a href=""><span class="menu-item-icon">🔍</span><span class="menu-item-text">検索</span></a></li>
+                    <li class="menu-item"><a href=""><span class="menu-item-icon">📸</span><span class="menu-item-text">発見</span></a></li>
+                    <li class="menu-item"><a href=""><span class="menu-item-icon">🎥</span><span class="menu-item-text">リール動画</span></li>
+                    <li class="menu-item"><a href="../chat_rooms.php"><img src="../img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>
+                    <li class="menu-item"><a href=""><span class="menu-item-icon">❤️</span><span class="menu-item-text">お知らせ</span></a></li>
+                    <li class="menu-item"><a href="p2_insert.php"><span class="menu-item-icon">➕</span><span class="menu-item-text">商品登録</span></a></li>
+                    <li class="menu-item"><a href=""><img src="../img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
+                </ul>
+            </div>
+          
+            <div>
+                <ul class="menu-list-bottom">
+                    <li class="menu-item"><a href=""><span class="menu-item-icon">💬</span><span class="menu-item-text">Threads</span></a></li>
+                    <li class="menu-item"><a href=""><img src="../img/haguruma.svg" class="logo"></span><span class="menu-item-text">その他</span></a></li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="right-content">
+        </div>
+    </div>
 </body>
 </html>
