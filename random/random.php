710bb729bc45738b5fcd328a36359c11046271f5
meru86 <rion160424@gmail.com>
2023-12-10 01:00:13 +0900

    ランダムなサンプルデータの挿入;

--- random/random.php
+++ random/random.php
@@ -1,23 +1,50 @@
 <?php
 include "db_config.php";
-//761 order
-//160 product
-//588
-$insertSql = "INSERT INTO orders_detail VALUES(?,?,?,?,?)";
-$insertStmt = $conn->prepare($insertSql);
-$order_id = rand(1,761);
-$product_id = rand(1,160);
-$color_size_id = rand(1,588);
-$order_pieces = rand(1,5);
 
-$select = "SELECT price FROM color_size
-            WHERE color_size_id = $color_size_id";
-$selectResult = $conn->query($select);
-$price;
-while($row = $selectResult->fetch_assoc()){
-    $price = $row['price'];
+for ($i = 0; $i < 2000; $i++) {
+    try {
+        $order_id = rand(1, 761);
+        $product_id = rand(1, 160);
+        $color_size_id = rand(1, 588);
+
+        // ここで重複がないか確認
+        $testSql = "SELECT product_id FROM orders_detail
+                    WHERE order_id = ? AND product_id = ? AND color_size_id = ?";
+        $testStmt = $conn->prepare($testSql);
+        $testStmt->bind_param("iii", $order_id, $product_id, $color_size_id);
+        $testStmt->execute();
+        $testResult = $testStmt->get_result();
+
+        if (!($testResult->num_rows > 0)) {
+            $order_pieces = rand(1, 5);
+
+            // トータルの値段取得
+            $selectSql = "SELECT price FROM color_size
+                           WHERE color_size_id = ?";
+            $selectStmt = $conn->prepare($selectSql);
+            $selectStmt->bind_param("i", $color_size_id);
+            $selectStmt->execute();
+            $selectResult = $selectStmt->get_result();
+
+            $price = 0;
+            while ($row = $selectResult->fetch_assoc()) {
+                $price = $row['price'];
+            }
+            $detail_total = $price * $order_pieces;
+
+            // insert
+            $insertSql = "INSERT INTO orders_detail VALUES(?, ?, ?, ?, ?)";
+            $insertStmt = $conn->prepare($insertSql);
+            $insertStmt->bind_param("iiiii", $order_id, $product_id, $color_size_id, $order_pieces, $detail_total);
+            
+            if ($insertStmt->execute()) {
+                echo "正常";
+            } else {
+                echo "Error in insert: " . $insertStmt->error;
+            }
+        }
+    } catch (Exception $e) {
+        echo "Error : " . $e->getMessage();
+    }
 }
-$detail_total = $price*$order_pieces;
-$insertStmt->bind_param("iiiii",$order_id,$product_id,$color_size_id,$order_pieces,$detail_total);
-$insertStmt->execute();
 ?>
