937f97994b40674175ada5a9257d1fee8bdec65f
meru86 <rion160424@gmail.com>
2023-12-10 11:53:59 +0900

    保険。多分マージしない;

--- seller/seller_products.php
+++ seller/seller_products.php
@@ -23,15 +23,17 @@ $seller_id = $_SESSION['seller_id'];
 $seller_name = $_SESSION['sellerName'];
 
 echo "<h1>登録済み商品一覧</h1>";
-echo "<h2>$seller_name 様</h2>"
+echo "<h2>$seller_name 様</h2>";
 
-$selectSql = "SELECT p.product_id, p.productname, p.view, p.create_at, p.seller_id, p.quality, s.color_code, s.size, s.pieces, s.price, s.color_size_id, b.big_category_name, c.category_name, small_category_name, i.img_url FROM products p
+$selectSql = "SELECT p.product_id, p.productname, p.view, p.create_at, p.seller_id, p.quality, s.color_code, s.size, s.pieces, s.price, s.color_size_id, b.big_category_name, c.category_name, sc.small_category_name, i.img_url 
+                FROM products p
                 LEFT JOIN color_size s ON (p.product_id = s.product_id)
                 LEFT JOIN big_category b ON (p.big_category_id = b.big_category_id)
                 LEFT JOIN category c ON (p.category_id = c.category_id)
                 LEFT JOIN small_category sc ON (p.small_category = sc.small_category)
                 LEFT JOIN products_img i ON (s.color_size_id = i.color_size_id)
                 WHERE p.seller_id = ?";
+
 $selectStmt = $conn->prepare($selectSql);
 $selectStmt->bind_param("s",$seller_id);
 $selectStmt->execute();
@@ -44,22 +46,24 @@ if($selectResult && $selectResult->num_rows > 0){
     $lastImg = array();
     while ($row = $selectResult->fetch_assoc()) {
         $imgText = null;
+        $product_id = $row['product_id'];
         $productname = $row['productname'];
-        $view = $row['view'];
+        $view = isset($row['view'])?$row['view']:"未登録";
         $create_at = $row['create_at'];
         $colorCode = $row['color_code'];
         $colorName = getColor($conn, $colorCode);
-        $product_id = $row['product_id'];
         $size = $row['size'];
         $pieces = $row['pieces'];
-        $category_name = !is_null($row['category_name'])?$row['category_name']:"";
+        $big_category_name = !is_null($row['big_category_name'])?$row['big_category_name']:"未登録";
+        $category_name = !is_null($row['category_name'])?$row['category_name']:"未登録";
+        $small_category_name = !is_null($row['small_category_name'])?$row['small_category_name']:"未登録";
         $price  = $row['price'];
         $color_size_id = $row['color_size_id'];
         $img_url = is_null($row['img_url'])?null:$row['img_url'];
         if(!is_null($img_url)){
             $imgText = "
             <!---<a href='productsDetail.php?product_id=$product_id&color_size_id=$color_size_id'>--->
-            <img src='seller/p_img/$img_url'>
+            <img src='p_img/$img_url'>
             <!---</a>----->";
         }//else{
             //ここで商品の画像が一枚もないときに表示する写真を表示するタブを作る。
@@ -76,9 +80,11 @@ if($selectResult && $selectResult->num_rows > 0){
             色: $colorName
             サイズ: $size<br>
             商品名　　: $productname<br>
-            カテゴリ名: $category_name<br>
+            カテゴリ名: $big_category_name - $category_name - $small_category_name<br>
+            概要　　　: $view<br>
             価格　　　: $price<br>
-            在庫数　　: $<br>
+            在庫数　　: $pieces<br>
+            出品日　　: $create_at<br>
             <!-----</a>---->
             <hr>
             END;
@@ -88,6 +94,7 @@ if($selectResult && $selectResult->num_rows > 0){
             echo $imgText;
         }
     }
+    echo $htmlText;
     echo "登録商品は".$count."件です。";
 }else{
     echo "登録されている商品がありません。";
