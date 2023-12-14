cbe88d0affe7d37050dfe34dd31b808d2b691316
revirs <yuuki0624kawano@gmail.com>
2023-12-14 10:51:25 +0900

    メニューバー追加

--- user_top.php
+++ user_top.php
@@ -13,33 +13,63 @@ error_reporting(E_ALL);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Amozon_insta.css">
     <title>ユーザーのトップ</title>
 </head>
 <body>
-    <h1>ユーザー側</h1>
-    <?php
-    if(isset($_GET['error_message'])){
-        $error_message = $_GET['error_message'];
-        echo "<p>".$error_message."</p>";
-    }
-    if(isset($_SESSION['user_id'])){
-        echo <<<END
-        <form action="logout.php" method="post">
-            <input type="submit" name="logout" value="ログアウト">
-        </form>
-        END;
-    }else{
-        echo <<<END
-        <a href="register.php">新規登録</a>
-        <a href="login.php">ログイン</a>
-        <br>
-        <br>
-        END;
-    }
-    ?>
+    
 
-    <a href="search.php">検索</a><br>
-    <a href="cartContents.php">カート</a><br>
-    <a href="chat_rooms.php">チャットルーム一覧</a>
+    
+    <div class="Amozon-container">
+        <!-- Left Side Menu -->
+        <div class="left-menu">
+            <div>
+                <ul class="menu-list">
+                    <li class="menu-item"><a href=""><img src="img/cart_dake.svg" class="logo"><span class="menu-item-text-logo">Re.ReaD</span></a></li>
+                    <li class="menu-item"><a href=""><img src="img/home.png" class="logo"></span><span class="menu-item-text">ホーム</span></a></li>
+                    <li class="menu-item"><a href="search.php"><span class="menu-item-icon">🔍</span><span class="menu-item-text">検索</span></a></li>
+                    <li class="menu-item"><a href="cartContents.php"><span class="menu-item-icon">📸</span><span class="menu-item-text">カート</span></a></li>
+                    <li class="menu-item"><a href=""><span class="menu-item-icon">🎥</span><span class="menu-item-text">リール動画</span></li>
+                    <li class="menu-item"><a href="chat_rooms.php"><img src="img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>
+                    <li class="menu-item"><a href=""><span class="menu-item-icon">❤️</span><span class="menu-item-text">お知らせ</span></a></li>
+                    <li class="menu-item"><a href=""><span class="menu-item-icon">➕</span><span class="menu-item-text">#</span></a></li>
+                    <li class="menu-item"><a href=""><img src="img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
+                </ul>
+            </div>
+            <div>
+                <ul class="menu-list-bottom">
+                    <li class="menu-item"><a href=""><span class="menu-item-icon">💬</span><span class="menu-item-text">Threads</span></a></li>
+                    <li class="menu-item"><a href=""><img src="img/haguruma.svg" class="logo"></span><span class="menu-item-text">その他</span></a></li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="right-content">
+            <h1>ユーザー側</h1>
+            <?php
+            if(isset($_GET['error_message'])){
+                $error_message = $_GET['error_message'];
+                echo "<p>".$error_message."</p>";
+            }
+            if(isset($_SESSION['user_id'])){
+                echo <<<END
+                <form action="logout.php" method="post">
+                    <input type="submit" name="logout" value="ログアウト">
+                </form>
+                END;
+            }else{
+                echo <<<END
+                <a href="register.php">新規登録</a>
+                <a href="login.php">ログイン</a>
+                <br>
+                <br>
+                END;
+            }
+            ?>
+            <a href="cartContents.php">カート</a><br>
+            <a href="chat_rooms.php">チャットルーム一覧</a>
+        </div>
+    </div>
+
 </body>
 </html>
