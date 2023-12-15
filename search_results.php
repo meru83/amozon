88276df69f216f075a6fb7b8b5551e29b55f735b
pikachu <yuuki0624kawano@gmail.com>
2023-12-15 13:44:44 +0900

    検索バーのデザイン

--- search_results.php
+++ search_results.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/search_style.css">
+    <title>商品検索</title>
+</head>
+
+<body>
+    <h1>商品検索</h1>
+    <form action="search_results.php" method="GET">
+        <label for="search">商品を検索：</label>
+        <input type="text" id="search" name="search">
+        <button type="submit">検索</button>
+    </form>
 <?php
 // データベース接続
 include "db_config.php";
