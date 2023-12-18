6c313506c724c15380a01108e5f1a97f4c8fe2a6
meru83 <rion160424@gmail.com>
2023-12-18 11:42:08 +0900

    リンクミス修正;

--- seller/seller_top.php
+++ seller/seller_top.php
@@ -25,7 +25,7 @@ error_reporting(E_ALL);
                     <li class="menu-item-logo"><a href=""><img src="../img/cart_dake.svg" class="logo"><span class="menu-item-text-logo">Re.ReaD</span></a></li>
                     <li class="menu-item"><a href=""><img src="../img/home.png" class="logo"><span class="menu-item-text">ホーム</span></a></li>
                     <li class="menu-item"><a href="../search.php"><img src="../img/musimegane.png" class="logo"><span class="menu-item-text">検索</span></a></li>
-                    <li class="menu-item"> <a href="../p2_insert.php"><img src="../img/cart.png" class="logo"><span class="menu-item-text">商品情報登録</span></a></li>
+                    <li class="menu-item"> <a href="p2_insert.php"><img src="../img/cart.png" class="logo"><span class="menu-item-text">商品情報登録</span></a></li>
                     <li class="menu-item"><a href="../chat_rooms.php"><img src="../img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>
                     <li class="menu-item"><a href=""><span class="menu-item-icon">❤️</span><span class="menu-item-text">お知らせ</span></a></li>
                     <li class="menu-item"><a href=""><span class="menu-item-icon">➕</span><span class="menu-item-text">#</span></a></li>
@@ -47,25 +47,24 @@ error_reporting(E_ALL);
                 $error_message = $_GET['error_message'];
                 echo "<p>".$error_message."</p>";
             }
-            if(isset($_SESSION['user_id'])){
+            if(isset($_SESSION['seller_id'])){
                 echo <<<END
-                <form action="logout.php" method="post">
+                <form action="seller_out.php" method="post">
                     <input type="submit" name="logout" value="ログアウト">
                 </form>
-                <a href="../seller_products.php">登録済み商品一覧</a><br>
+                <a href="seller_products.php">登録済み商品一覧</a><br>
                 <a href="../chat_rooms.php">チャットルーム一覧</a>
                 END;
             }else{
                 echo <<<END
-                <a href="../register.php">新規登録</a>
-                <a href="../login.php">ログイン</a><br>
+                <a href="seller.php">新規登録</a>
+                <a href="seller_log.php">ログイン</a><br>
                 ユーザー登録またはログインを完了させてください。
                 <br>
                 <br>
                 END;
             }
             ?>
-            <a href="../chat_rooms.php">チャットルーム一覧</a>
         </div>
     </div>
 
