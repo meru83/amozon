ceabcef879de3559156c0abc11594f259360ee14
river-s <yuuki0624kawano@mail.com>
2023-12-19 21:29:29 +0900

    販売者側のメニューバー追加

--- chat_rooms.php
+++ chat_rooms.php
@@ -96,6 +96,30 @@ if ($result === false) {
         HTML;
     }else if(!is_null($seller_id)){
         //seller
+        echo <<< HTML
+        <div class="Amozon-container">
+        <!-- Left Side Menu -->
+        <div class="left-menu">
+            <div>
+                <ul class="menu-list">
+                    <li class="menu-item-logo"><a href=""><img src="../img/cart_dake.svg" class="logo"><span class="menu-item-text-logo">Re.ReaD</span></a></li>
+                    <li class="menu-item"><a href=""><img src="../img/home.png" class="logo"><span class="menu-item-text">ホーム</span></a></li>
+                    <li class="menu-item"><a href="../search.php"><img src="../img/musimegane.png" class="logo"><span class="menu-item-text">検索</span></a></li>
+                    <li class="menu-item"> <a href="p2_insert.php"><img src="../img/cart.png" class="logo"><span class="menu-item-text">商品情報登録</span></a></li>
+                    <li class="menu-item"><a href="../chat_rooms.php"><img src="../img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>
+                    <li class="menu-item"><a href=""><span class="menu-item-icon">❤️</span><span class="menu-item-text">お知らせ</span></a></li>
+                    <li class="menu-item"><a href=""><span class="menu-item-icon">➕</span><span class="menu-item-text">#</span></a></li>
+                    <li class="menu-item"><a href=""><img src="../img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
+                </ul>
+            </div>
+            <div>
+                <ul class="menu-list-bottom">
+                    <li class="menu-item"><a href=""><span class="menu-item-icon">💬</span><span class="menu-item-text">Threads</span></a></li>
+                    <li class="menu-item"><a href=""><img src="../img/haguruma.svg" class="logo"></span><span class="menu-item-text">その他</span></a></li>
+                </ul>
+            </div>
+        </div>
+        HTML;
     }
          
     echo '<div class="right-content">';
