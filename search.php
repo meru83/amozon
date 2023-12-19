3b4a7923722895fa4ad5c495911c04b7bb0faff7
meru <rion160424@gmail.com>
2023-12-20 00:03:05 +0900

    空白などの検索無効化;

--- search.php
+++ search.php
@@ -10,17 +10,28 @@
 
 <body>
     <h1>商品検索</h1>
-    <form action="search_results.php" method="GET">
+    <form id="form" action="search_results.php" method="GET">
     <div class="flexBox">
         <label for="search">商品を検索</label>
         <input type="text" id="search" name="search">
         <button type="submit" id="submit" class="btn-img"></button>
-        </div>
+    </div>
     </form>
 </body>
 </html>
-<!-- <script>
+<script>
 const search = document.getElementById('search');
 const submit = document.getElementById('submit');
-submit.addEventListener('submit',);
-</script> -->
+const form = document.getElementById('form');
+form.addEventListener('submit',(e) => {
+    let searchValue = search.value;
+    let str = searchValue.replace(/\s+/g, "");
+    console.log(str);
+    if(str === null || str === ""){
+        e.preventDefault();
+        return false;
+    }else{
+        return true;
+    }
+});
+</script>
