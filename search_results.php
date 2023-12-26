e1d9895f8429e86c572b5ec5b21e422819c827b3
meru83 <rion160424@gmail.com>
2023-12-26 09:59:26 +0900

    div完成;

--- search_results.php
+++ search_results.php
@@ -38,7 +38,7 @@ if(!empty($searchText)  && !in_array($searchText, ['新品', '未使用', '新�
         $orKeywords = array($searchText);
     }
 
-    echo "<div class='all'>";
+    echo "<div class='all'>";//全体
     foreach($orKeywords as $orKeyword){
         $conditions = array();
         $qualityConditions = array();
@@ -88,6 +88,8 @@ if(!empty($searchText)  && !in_array($searchText, ['新品', '未使用', '新�
             // 検索結果を表示
             if ($result->num_rows > 0) {
                 $lastImg = array();
+                echo '<div class="productAll">';
+                echo '<div class="imgAll">';
                 while ($row = $result->fetch_assoc()) {
                     $imgText = null;
                     $colorCode = $row['color_code'];
@@ -110,7 +112,11 @@ if(!empty($searchText)  && !in_array($searchText, ['新品', '未使用', '新�
                     //}
                     //画像にサイズと色の説明が出るようにする。
                     if(!in_array($color_size_id, $lastImg)){
+                        echo '</div>';
                         echo $htmlText;
+                        echo '</div>';
+                        echo '<div class="productAll">';
+                        echo '<div class="imgAll">';
                         echo $imgText;
                         $lastImg[] = $color_size_id;
                         $htmlText = <<<END
@@ -140,8 +146,9 @@ if(!empty($searchText)  && !in_array($searchText, ['新品', '未使用', '新�
                         echo $imgText;
                     }
                 }
+                echo '</div>';
                 echo $htmlText;
-                echo "</div>";//全体
+                echo '</div>';
             } else {
                 echo "該当する商品がありません。<br>";
             }
@@ -149,7 +156,7 @@ if(!empty($searchText)  && !in_array($searchText, ['新品', '未使用', '新�
             die('クエリ実行に失敗しました: ' . $conn->error);
         }
     }
-    echo "</div>";
+    echo "</div>";//全体
     echo "該当商品が" . $count . "件見つかりました。";
 }else if(empty($searchText)){
     echo "検索キーワードを入力してください。<br>";
