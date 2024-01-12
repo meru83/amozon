1260faf00f01f28042289f4306c64210c7c43745
meru83 <rion160424@gmail.com>
2024-01-12 14:22:34 +0900

    途中;

--- cartContents.php
+++ cartContents.php
@@ -171,38 +171,8 @@ if(isset($_SESSION['user_id'])){
     if($count !== 0) {
         echo $count . "件";
     }else{
-        echo <<<HTML
-        <!-- <div id="header" class="header">
-            <div class="space"></div>
-            <h1 class="h1_White">トップページ</h1>
-            <div class="space"></div>
-        </div>
-        <div class="Amozon-container"> -->
-        <!-- Left Side Menu -->
-        <!-- <div class="left-menu">
-            <div>
-                <ul class="menu-list">
-                    <li class="menu-item-logo"><a href=""><img src="img/cart_dake.svg" class="logo"><span class="menu-item-text-logo">Re.ReaD</span></a></li>
-                    <li class="menu-item"><a href=""><img src="img/home.png" class="logo"><span class="menu-item-text">ホーム</span></a></li>
-                    <li class="menu-item"><a href="search.php"><img src="img/musimegane.png" class="logo"><span class="menu-item-text">検索</span></a></li>
-                    <li class="menu-item"><a href="cartContents.php"><img src="img/cart.png" class="logo"><span class="menu-item-text">カート</span></a></li>
-                    <li class="menu-item"><a href="chat_rooms.php"><img src="img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>
-                    <li class="menu-item"><a href=""><span class="menu-item-icon">❤️</span><span class="menu-item-text">お知らせ</span></a></li>
-                    <li class="menu-item"><a href=""><img src="img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
-                </ul>
-            </div>
-            <div>
-                <ul class="menu-list-bottom">
-                    <li class="menu-item"><a href=""><img src="img/haguruma.svg" class="logo"></span><span class="menu-item-text">その他</span></a></li>
-                </ul>
-            </div>
-        </div>
-        
-            <div class="right-content"> -->
-        <!-- </div> -->
-        HTML;
+        //0件
     }
-    //echo </div>;
 
     echo <<<END
     <script>
