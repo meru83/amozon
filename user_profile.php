4b3c9d8bfdbfb16c33b9931675f3c911903c6b0a
rairu <2322086@i-seifu.jp>
2024-01-12 09:34:29 +0900

    コメントアウトした;

--- user_profile.php
+++ user_profile.php
@@ -33,7 +33,7 @@
                     <img src="img/cart_dake.svg" class="amozon_usericon">
                     <h1>ユーザー名</h1>
                     <p>こんにちは、私はユーザー名です。プロフィールの説明文がここに入ります。</p>
-                    <div class="sub-content">
+                    <!-- <div class="sub-content">
                         <div class="sub-content-item">
                             <h2>サブコンテンツ1</h2>
                             <p>ここにサブコンテンツ1の説明が入ります。</p>
@@ -45,7 +45,7 @@
                     <div class="sub-content-item">
                             <h2>サブコンテンツ3</h2>
                             <p>ここにサブコンテンツ3の説明が入ります。</p>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
