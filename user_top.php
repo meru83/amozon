0a1cd57384b0baa078491eb95e52279f424c8965
sese <2312091@i-seifu.jp>
2024-01-12 14:46:32 +0900

    表示変更;

--- user_top.php
+++ user_top.php
@@ -47,19 +47,18 @@ if(isset($_SESSION['user_id'])){
                     <li class="menu-item"><a href="search.php"><img src="img/musimegane.png" class="logo"><span class="menu-item-text">検索</span></a></li>
                     <li class="menu-item"><a href="cartContents.php"><img src="img/cart.png" class="logo"><span class="menu-item-text">カート</span></a></li>
                     <li class="menu-item"><a href="chat_rooms.php"><img src="img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>
-                    <li class="menu-item"><a href=""><span class="menu-item-icon">❤️</span><span class="menu-item-text">お知らせ</span></a></li>
+                    <li class="menu-item"><a href=""><span class="menu-item-icon">❤️</span><span class="menu-item-text">お気に入り</span></a></li>
                     <li class="menu-item"><a href=""><img src="img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
                 </ul>
             </div>
             <div>
                 <ul class="menu-list-bottom">
-                    <li class="menu-item"><a href=""><img src="img/haguruma.svg" class="logo"></span><span class="menu-item-text">その他</span></a></li>
                 </ul>
             </div>
         </div>
         
         <div class="right-content">
-            <h1>ユーザー側</h1>
+            <h1></h1>
             <?php
             if(isset($_GET['error_message'])){
                 $error_message = $_GET['error_message'];
