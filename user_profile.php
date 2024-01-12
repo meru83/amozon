9959625b0e377cb47a931aa1483edefeb0e02149
rairu <2322086@i-seifu.jp>
2024-01-12 12:15:16 +0900

    プロフィール画面にヘッダー追加;

--- user_profile.php
+++ user_profile.php
@@ -7,23 +7,26 @@
     <title>プロフィール</title>
 </head>
 <body>
+    <div id="header" class="header">
+        <div class="space"></div>
+        <h1 class="h1_White">プロフィール</h1>
+        <div class="space"></div>
+    </div>
 <div class="profile_container">    
-        <div class="left-menu">
+<div class="left-menu">
             <div>
                 <ul class="menu-list">
                     <li class="menu-item-logo"><a href=""><img src="img/cart_dake.svg" class="logo"><span class="menu-item-text-logo">Re.ReaD</span></a></li>
                     <li class="menu-item"><a href=""><img src="img/home.png" class="logo"><span class="menu-item-text">ホーム</span></a></li>
-                    <li class="menu-item"><a href="../search.php"><img src="img/musimegane.png" class="logo"><span class="menu-item-text">検索</span></a></li>
-                    <li class="menu-item"> <a href="p2_insert.php"><img src="img/cart.png" class="logo"><span class="menu-item-text">商品情報登録</span></a></li>
-                    <li class="menu-item"><a href="../chat_rooms.php"><img src="img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>
+                    <li class="menu-item"><a href="search.php"><img src="img/musimegane.png" class="logo"><span class="menu-item-text">検索</span></a></li>
+                    <li class="menu-item"><a href="cartContents.php"><img src="img/cart.png" class="logo"><span class="menu-item-text">カート</span></a></li>
+                    <li class="menu-item"><a href="chat_rooms.php"><img src="img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>
                     <li class="menu-item"><a href=""><span class="menu-item-icon">❤️</span><span class="menu-item-text">お知らせ</span></a></li>
-                    <li class="menu-item"><a href=""><span class="menu-item-icon">➕</span><span class="menu-item-text">#</span></a></li>
                     <li class="menu-item"><a href=""><img src="img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
                 </ul>
             </div>
             <div>
                 <ul class="menu-list-bottom">
-                    <li class="menu-item"><a href=""><span class="menu-item-icon">💬</span><span class="menu-item-text">Threads</span></a></li>
                     <li class="menu-item"><a href=""><img src="img/haguruma.svg" class="logo"></span><span class="menu-item-text">その他</span></a></li>
                 </ul>
             </div>
