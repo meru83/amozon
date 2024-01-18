6ab61f6c4ba680013b417c8e24a7777fd8ffde94
RAIRU <2322086@i-seifu.jp>
2024-01-18 13:03:42 +0900

    プロフィールにリンク張った;

--- chat_rooms.php
+++ chat_rooms.php
@@ -105,7 +105,7 @@ if(isset($_SESSION['seller_id'])){
                     <li class="menu-item"><a href="cartContents.php"><img src="img/cart.png" class="logo"><span class="menu-item-text">カート</span></a></li>
                     <li class="menu-item"><a href="chat_rooms.php"><img src="img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>
                     <li class="menu-item"><a href="favoriteProduct.php"><span class="menu-item-icon">❤️</span><span class="menu-item-text">お気に入り</span></a></li>
-                    <li class="menu-item"><a href=""><img src="img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
+                    <li class="menu-item"><a href="user_profile.php"><img src="img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
                 </ul>
             </div>
             <div>
@@ -127,7 +127,7 @@ if(isset($_SESSION['seller_id'])){
                     <li class="menu-item"> <a href="seller/p2_insert.php"><img src="img/cart.png" class="logo"><span class="menu-item-text">商品情報登録</span></a></li>
                     <li class="menu-item"> <a href="seller/seller_products.php"><img src="img/cart.png" class="logo"><span class="menu-item-text">登録商品一覧</span></a></li>
                     $seller_chat
-                    <li class="menu-item"><a href=""><img src="img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
+                    <li class="menu-item"><a href="user_profile.php"><img src="img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
                     <!--log--->
                 </ul>
             </div>
