747ed19cbfa95f345c79c863e65705b6421ade0a
river-s <yuuki0624kawano@mail.com>
2024-01-21 21:18:45 +0900

    リンク

--- user_profile.php
+++ user_profile.php
@@ -31,44 +31,46 @@ if(isset($_SESSION['user_id'])){
         <?=$foo2?>
     </div>
 <div class="profile_container">    
-<div class="left-menu">
-            <div>
-                <ul class="menu-list">
-                    <li class="menu-item-logo"><a href=""><img src="img/cart_dake.svg" class="logo"><span class="menu-item-text-logo">Re.ReaD</span></a></li>
-                    <li class="menu-item"><a href="user_top.php"><img src="img/home.png" class="logo"><span class="menu-item-text">ホーム</span></a></li>
-                    <li class="menu-item"><a href="search.php"><img src="img/musimegane.png" class="logo"><span class="menu-item-text">検索</span></a></li>
-                    <li class="menu-item"><a href="cartContents.php"><img src="img/cart.png" class="logo"><span class="menu-item-text">カート</span></a></li>
-                    <li class="menu-item"><a href="chat_rooms.php"><img src="img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>
-                    <li class="menu-item"><a href="favoriteProduct.php"><img src="img/heartBlack.png" class="logo"><span class="menu-item-text">お気に入り</span></a></li>
-                    <li class="menu-item"><a href="user_profile.php"><img src="img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
-                </ul>
-            </div>
-            <div>
-                <ul class="menu-list-bottom">
-                </ul>
-            </div>
+    <div class="left-menu">
+        <div>
+            <ul class="menu-list">
+                <li class="menu-item-logo"><a href=""><img src="img/cart_dake.svg" class="logo"><span class="menu-item-text-logo">Re.ReaD</span></a></li>
+                <li class="menu-item"><a href="user_top.php"><img src="img/home.png" class="logo"><span class="menu-item-text">ホーム</span></a></li>
+                <li class="menu-item"><a href="search.php"><img src="img/musimegane.png" class="logo"><span class="menu-item-text">検索</span></a></li>
+                <li class="menu-item"><a href="cartContents.php"><img src="img/cart.png" class="logo"><span class="menu-item-text">カート</span></a></li>
+                <li class="menu-item"><a href="chat_rooms.php"><img src="img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>
+                <li class="menu-item"><a href="favoriteProduct.php"><img src="img/heartBlack.png" class="logo"><span class="menu-item-text">お気に入り</span></a></li>
+                <li class="menu-item"><a href="user_profile.php"><img src="img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
+            </ul>
         </div>
-            <div class="right-content">
-                <div class="amozon_profile">
-                    <img src="img/cart_dake.svg" class="amozon_usericon">
-                    <h1>ユーザー名</h1>
-                    <p>こんにちは、私はユーザー名です。プロフィールの説明文がここに入ります。</p>
-                    <!-- <div class="sub-content">
-                        <div class="sub-content-item">
-                            <h2>サブコンテンツ1</h2>
-                            <p>ここにサブコンテンツ1の説明が入ります。</p>
-                    </div>
+        <div>
+            <ul class="menu-list-bottom">
+            </ul>
+        </div>
+    </div>
+    <div class="right-content">
+        <div class="amozon_profile">
+            <img src="img/cart_dake.svg" class="amozon_usericon">
+            <h1>ユーザー名</h1>
+            <p>こんにちは、私はユーザー名です。プロフィールの説明文がここに入ります。</p>
+            <div class="sub-content">
+                <a href="address_insert.php">
                     <div class="sub-content-item">
-                            <h2>サブコンテンツ2</h2>
-                            <p>ここにサブコンテンツ2の説明が入ります。</p>
+                        <h2>住所登録</h2>
+                        <p>ここにサブコンテンツ1の説明が入ります。</p>
                     </div>
-                    <div class="sub-content-item">
-                            <h2>サブコンテンツ3</h2>
-                            <p>ここにサブコンテンツ3の説明が入ります。</p>
-                    </div> -->
+                </a>
+                <div class="sub-content-item">
+                    <h2>サブコンテンツ2</h2>
+                    <p>ここにサブコンテンツ2の説明が入ります。</p>
+                </div>
+                <div class="sub-content-item">
+                    <h2>サブコンテンツ3</h2>
+                    <p>ここにサブコンテンツ3の説明が入ります。</p>
                 </div>
             </div>
         </div>
+    </div>
 </div>
 </body>
 </html>
