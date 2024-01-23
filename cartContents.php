b400be4baeaa5aa3d19e8fad44fbf9eb1b1bf547
RAIRU\tanim <2322086@i-seifu.jp>
2024-01-23 18:54:05 +0900

    ヘッダーの名前修正; サイズの横幅バグ修正;

--- cartContents.php
+++ cartContents.php
@@ -56,7 +56,7 @@ if(isset($_SESSION['user_id'])){
 
         <div id="header" class="header">
             <div class="back"><div class="backBtn" onclick="history.back()"><img src="img/return_left.png" style="width:100%;"></div></div>
-            <h1 class="h1_White">トップページ</h1>
+            <h1 class="h1_White">カート</h1>
             <?=$foo2?>
         </div>
 
