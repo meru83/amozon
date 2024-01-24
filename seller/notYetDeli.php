f2c206005a4774f3178376fb09f7644e96cbee47
river-s <yuuki0624kawano@mail.com>
2024-01-24 17:26:07 +0900

    css

--- seller/notYetDeli.php
+++ seller/notYetDeli.php
@@ -20,6 +20,95 @@ if(!isset($_SESSION['seller_id'])){
 session_regenerate_id(TRUE);
 $seller_id = $_SESSION['seller_id'];
 
+if(isset($_SESSION['seller_id'])){
+    $seller_id = $_SESSION['seller_id'];
+    $foo = <<<END
+    <div style="width:100%; text-align: right; height: fit-content;">
+    <form action="seller_out.php" method="post">
+        <input type="submit" name="logout" class="log_out" value="ログアウト">
+    </form>
+    </div>
+    END;
+}else{
+    // $foo = <<<END
+    // <div class="New_log">
+    //     <a href="seller.php"><div class="log_style">新規登録</div></a>
+    //     <a href="seller_log.php"><div class="log_style rightM">ログイン</div></a>
+    // </div>
+    // END;
+    header("Location:seller_log.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/notYet.css">
+</head>
+<body>
+<div id="header" class="header">
+        <div class="back"><div class="backBtn" onclick="history.back()"><img src="../img/return_left.png" style="width:100%;"></div></div>
+        <h1 class="h1_White">販売者側トップページ</h1>
+        <?=$foo?>
+    </div>
+
+        <div class="Amozon-container">
+        <!-- Left Side Menu -->
+            <div class="left-menu">
+                <div>
+                    <ul class="menu-list">
+                    <li class="menu-item-logo"><a href="" class="a_link"><img src="../img/cart_dake.svg" class="logo"><span class="menu-item-text-logo">Re.ReaD</span></a></li>
+                    <li class="menu-item"><a href="seller_top.php" class="a_link"><img src="../img/home.png" class="logo"><span class="menu-item-text">ホーム</span></a></li>
+                    <li class="menu-item"> <a href="p2_insert.php" class="a_link"><img src="../img/hensyu.png" class="logo"><span class="menu-item-text">商品情報登録</span></a></li>
+                    <li class="menu-item"> <a href="seller_products.php" class="a_link"><img src="../img/meisi.png" class="logo"><span class="menu-item-text">登録商品一覧</span></a></li>
+                    <!-- <li class="menu-item"> <a href=""><img src="../img/meisi.png" class="logo"><span class="menu-item-text">未発送商品</span></a></li> -->
+                    <?php
+                    $notYetSql = "SELECT COUNT(DISTINCT o.order_id) AS notYetDeli FROM orders o
+                                LEFT JOIN orders_detail d ON (o.order_id = d.order_id)
+                                LEFT JOIN products p ON (d.product_id = p.product_id)
+                                WHERE o.order_status = '出荷準備中' && p.seller_id = ?";
+                    $notYetStmt = $conn->prepare($notYetSql);
+                    $notYetStmt->bind_param("s",$seller_id);
+                    $notYetStmt->execute();
+                    $notYetResult = $notYetStmt->get_result();
+                    if($notYetResult && $notYetResult->num_rows > 0){
+                        $notYetRow = $notYetResult->fetch_assoc();
+                        $notYetDeli = $notYetRow['notYetDeli'];
+                        echo <<<HTML
+                        <li class="menu-item"> <a href="notYetDeli.php" class="a_link"><img src="../img/meisi.png" class="logo"><span class="menu-item-text">未発送商品</span><span class="tuuti">$notYetDeli</span></a></li>
+                        HTML;
+                    }else{
+                        echo <<<HTML
+                        <li class="menu-item"> <a href="notYetDeli.php" class="a_link"><img src="../img/meisi.png" class="logo"><span class="menu-item-text">未発送商品</span></a></li>
+                        HTML;
+                    }
+
+                    if(isset($_SESSION['seller_id'])){
+                        echo '<li class="menu-item"><a href="../chat_rooms.php" class="a_link"><img src="../img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>';
+                    }else{
+                        echo '<li class="menu-item"><a href="seller.php" class="a_link"><img src="../img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>';
+                    }
+                    ?>
+                    <li class="menu-item"><a href="seller_home.php" class="a_link"><img src="../img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
+                    <!--log--->
+                </ul>
+            </div>
+            <div>
+                <ul class="menu-list-bottom">
+                <li class="menu-item"><a href="../py/rireki.php" class="a_link"><img src="../img/gurafu.png" class="logo"><span class="menu-item-text">売上管理</span></a></li>
+                    </ul>
+                </div>
+                <div>
+                    <ul class="menu-list-bottom">
+                    </ul>
+                </div>
+            </div>
+            <div class="right-content">
+
+<?php
 
 $arrayOrderId = array();
 $htmlText = "";
@@ -58,8 +147,8 @@ if($notYetDeliResult && $notYetDeliResult->num_rows > 0){
         $address_id = $notYetDeliRow['address_id'];
 
         $htmlText = <<<HTML
-        <b>商品ID</b>$product_id$color_size_id
-        $productname $colorName $size<div>単価$price  ＊ $order_pieces  </div><div>計　$detail_total</div>
+        <div class='backG'><b>商品ID</b>$product_id$color_size_id<br>
+        <b>商品名</b>$productname $colorName $size<div>単価$price  ＊ $order_pieces  </div><div>計　$detail_total</div>
         HTML;
 
         if(!in_array($order_id,$arrayOrderId)){
@@ -71,9 +160,10 @@ if($notYetDeliResult && $notYetDeliResult->num_rows > 0){
             <br>
             <b>購入日時</b>：$create_at 
             <a href="../user_profile.php">$user_id</a> 様
-            <button id="$product_id$color_size_id" type="button" style="display:block" onclick="deliComplete($product_id$color_size_id, $order_id, '$user_id')">発送完了</button>
-            <div id="2$product_id$color_size_id" style="display:none">発送が完了しました</div>
-            <hr>
+            <button id="$product_id$color_size_id" class="hasso" type="button" style="display:block" onclick="deliComplete($product_id$color_size_id, $order_id, '$user_id')">発送完了</button>
+            <div id="2$product_id$color_size_id" style="display:none; color:red; margin-top:24px;">発送が完了しました</div>
+            </div>
+            <span style="display:block; height:10px; background-color:white;"></span>
             END;
         }else{
             echo $htmlText;
@@ -84,6 +174,11 @@ if($notYetDeliResult && $notYetDeliResult->num_rows > 0){
 }else{
     //未発送の商品がないとき
 }
+
+echo '</div>';//<div class="right-content">
+echo '</div>';//<div class="Amozon-container">
+echo '</body>';
+echo '</html>';
 
 function getColor($conn, $color_code){
     $colorSql = "SELECT * FROM color_name
