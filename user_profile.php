6bb22529d64f7213659d02dd63afa270f5268393
meru86 <rion160424@gmail.com>
2024-01-26 03:00:09 +0900

    要確認

--- user_profile.php
+++ user_profile.php
@@ -47,6 +47,9 @@ END;
     <div class="left-menu">
         <div>
             <ul class="menu-list">
+            <?php
+            if(isset($_SESSION['user_id'])){
+                echo <<<HTML
                 <li class="menu-item-logo"><a href="" class="a_link"><img src="img/cart_dake.svg" class="logo"><span class="menu-item-text-logo">Re.ReaD</span></a></li>
                 <li class="menu-item"><a href="user_top.php" class="a_link"><img src="img/home.png" class="logo"><span class="menu-item-text">ホーム</span></a></li>
                 <li class="menu-item"><a href="search.php" class="a_link"><img src="img/musimegane.png" class="logo"><span class="menu-item-text">検索</span></a></li>
@@ -54,14 +57,55 @@ END;
                 <li class="menu-item"><a href="chat_rooms.php" class="a_link"><img src="img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>
                 <li class="menu-item"><a href="favoriteProduct.php" class="a_link"><img src="img/heartBlack.png" class="logo"><span class="menu-item-text">お気に入り</span></a></li>
                 <li class="menu-item"><a href="buyHistory.php" class="a_link"><img src="img/meisi.png" class="logo"><span class="menu-item-text">購入履歴</span></a></li>
-                <?php
+                HTML;
                 if(isset($_SESSION['user_id'])){
                     $flagUserId = $_SESSION['user_id'];
                     echo <<<HTML
-                    <li class="menu-item"><a href="user_profile.php?user_id=$flagUserId" class="a_link"><img src="img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
+                    <li class="menu-item"><a href="user_profile.php?user_id=$flagUserId" class="a_link"><img src="../img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
                     HTML;
                 }
-                ?>
+            }else{
+                echo <<<HTML
+                <li class="menu-item-logo"><a href=""><img src="img/cart_dake.svg" class="logo"><span class="menu-item-text-logo">Re.ReaD</span></a></li>
+                <li class="menu-item"><a href="seller/seller_top.php"><img src="img/home.png" class="logo"><span class="menu-item-text">ホーム</span></a></li>
+                <li class="menu-item"> <a href="seller/p2_insert.php"><img src="img/hensyu.png" class="logo"><span class="menu-item-text">商品情報登録</span></a></li>
+                <li class="menu-item"> <a href="seller/seller_products.php"><img src="img/meisi.png" class="logo"><span class="menu-item-text">登録商品一覧</span></a></li>
+                HTML;
+                $notYetSql = "SELECT COUNT(DISTINCT o.order_id) AS notYetDeli FROM orders o
+                LEFT JOIN orders_detail d ON (o.order_id = d.order_id)
+                LEFT JOIN products p ON (d.product_id = p.product_id)
+                WHERE o.order_status = '出荷準備中' && p.seller_id = ?";
+                $notYetStmt = $conn->prepare($notYetSql);
+                $notYetStmt->bind_param("s",$seller_id);
+                $notYetStmt->execute();
+                $notYetResult = $notYetStmt->get_result();
+                if($notYetResult && $notYetResult->num_rows > 0){
+                    $notYetRow = $notYetResult->fetch_assoc();
+                    $notYetDeli = $notYetRow['notYetDeli'];
+                    echo <<<HTML
+                    <li class="menu-item"> <a href="seller/notYetDeli.php"><img src="img/kuruma.png" class="logo"><span class="menu-item-text">未発送商品</span><span class="tuuti">$notYetDeli</span></a></li>
+                    HTML;
+                }else{
+                    echo <<<HTML
+                    <li class="menu-item"> <a href="seller/notYetDeli.php"><img src="img/kuruma.png" class="logo"><span class="menu-item-text">未発送商品</span></a></li>
+                    HTML;
+                }
+                // <?php
+                if(isset($_SESSION['seller_id'])){
+                    echo '<li class="menu-item"><a href="chat_rooms.php"><img src="img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>';
+                }else{
+                    echo '<li class="menu-item"><a href="seller/seller.php"><img src="img/chat2.svg" class="logo"></span><span class="menu-item-text-chat">メッセージ</span></a></li>';
+                }
+                // 
+                // <?php
+                if(isset($_SESSION['seller_id'])){
+                    $flagSellerId = $_SESSION['seller_id'];
+                    echo <<<HTML
+                    <li class="menu-item"><a href="seller/seller_profile.php?seller_id=$flagSellerId"><img src="img/hito.png" class="logo"><span class="menu-item-text">プロフィール</span></a></li>
+                    HTML;
+                }
+            }
+            ?>
             </ul>
         </div>
         <div>
