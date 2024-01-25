723384c300ba10abc2110f69bb6eaaf372b00a7c
RAIRU\tanim <2322086@i-seifu.jp>
2024-01-25 18:59:03 +0900

    セラー側のサブメニュー画像張替え;

--- seller/p2_insert.php
+++ seller/p2_insert.php
@@ -71,11 +71,11 @@ if(isset($_SESSION['seller_id'])){
                         $notYetRow = $notYetResult->fetch_assoc();
                         $notYetDeli = $notYetRow['notYetDeli'];
                         echo <<<HTML
-                        <li class="menu-item"> <a href="notYetDeli.php"><img src="../img/meisi.png" class="logo"><span class="menu-item-text">未発送商品</span><span class="tuuti">$notYetDeli</span></a></li>
+                        <li class="menu-item"> <a href="notYetDeli.php"><img src="../img/kuruma.png" class="logo"><span class="menu-item-text">未発送商品</span><span class="tuuti">$notYetDeli</span></a></li>
                         HTML;
                     }else{
                         echo <<<HTML
-                        <li class="menu-item"> <a href="notYetDeli.php"><img src="../img/meisi.png" class="logo"><span class="menu-item-text">未発送商品</span></a></li>
+                        <li class="menu-item"> <a href="notYetDeli.php"><img src="../img/kuruma.png" class="logo"><span class="menu-item-text">未発送商品</span></a></li>
                         HTML;
                     }
 
